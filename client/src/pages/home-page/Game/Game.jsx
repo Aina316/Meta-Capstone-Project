5989abec1ee57f68e2e63f8f6db8a2312fe40f67
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-27 18:51:34 -0700

    feat: Allow users to view catalog of games available

--- client/src/pages/home-page/Game/Game.jsx
+++ client/src/pages/home-page/Game/Game.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
-
+import "./Game.css";
 const Game = ({ game }) => {
   return (
     <div className="game-component">
       <Link to={`/games/${game.id}`}></Link>
-      <img src={game.mage} alt={game.title} className="game-image" />
+      <img src={game.cover_image} alt={game.title} className="game-image" />
       <h3>{game.title}</h3>
       <p>{game.platform}</p>
-      <p>Owner: {game.owner.username}</p>
     </div>
   );
 };
