200d4a3d387ef5e07ba10fbc017139ed9605f924
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-29 22:51:26 -0700

    Feat: Implement Request Board and Request Components UI

--- client/src/pages/home-page/Request/Request.jsx
+++ client/src/pages/home-page/Request/Request.jsx
@@ -1,4 +1,26 @@
-const Request = () => {
-  <div className="request-component"></div>;
+import "./RequestBoard.css";
+
+const Request = ({ request }) => {
+  return (
+    <div className="request-component">
+      <div className="request-info">
+        <img
+          className="request-avatar"
+          src={request.avatar}
+          alt={request.username}
+        />
+        <div className="request-details">
+          <h4>{request.username}</h4>
+          <p>Borrower Score: {request.borrowerScore}</p>
+          <p className="request-date">{request.date}</p>
+        </div>
+      </div>
+      <div className="request-actions">
+        <button className="accept-btn">Accept</button>
+        <button className="decline-btn">Decline</button>
+      </div>
+    </div>
+  );
 };
+
 export default Request;
--- client/src/pages/home-page/Request/RequestBoard.jsx
+++ client/src/pages/home-page/Request/RequestBoard.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import "./RequestBoard.css";
 import Request from "./Request";
 const RequestBoard = () => {
-  return <div className="request-board-component"></div>;
+  const [requests, setRequests] = useState([]);
+
+  return (
+    <div className="request-board-component">
+      <h2>Borrow Requests</h2>
+      {requests.length == 0 ? (
+        <p>No requests right now!</p>
+      ) : (
+        <div className="request-list">
+          {setRequests.map((req) => (
+            <Request key={req.id} request={req} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RequestBoard;
