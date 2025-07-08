64f6174574eb07d1bc79020f40583eae71b35ee9
Emmanuel Aina <emmanuelaina04@meta.com>
2025-07-07 22:28:03 -0700

    Feat: Adds function to extract all unique genre

--- client/src/services/catalogService.js
+++ client/src/services/catalogService.js
@@ -16,3 +16,22 @@ export const fetchCatalogGameById = async (catalogId) => {
     .single();
   return { data, error };
 };
+
+//function to extract all possible genres for games in catalog table
+export const fetchAllGenres = async () => {
+  const { data, error } = await supabase.from("catalog").select("genre");
+
+  if (error) {
+    console.error("Error fetching genres: ", error);
+    return [];
+  }
+
+  const allGenres = data
+    .flatMap((row) =>
+      row.genre ? row.genre.split(",").map((g) => g.trim()) : []
+    )
+    .filter(Boolean);
+
+  const uniqueGenres = Array.from(new Set(allGenres));
+  return uniqueGenres.sort();
+};
