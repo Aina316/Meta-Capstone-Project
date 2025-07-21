8e72f65843dd8fd3dfca7752a1b8b0757867654d
Emmanuel Aina <emmanuelaina04@meta.com>
2025-07-21 12:57:20 -0700

    Feat: Creates Popup Message modal and imports toastify package for user messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import PrivateRoute from "./routes/PrivateRoute";
 import Landing from "./pages/landing-page/Landing";
 import Login from "./pages/login-page/Login";
@@ -15,77 +16,80 @@ import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/newuser"
-        element={
-          <PrivateRoute>
-            <NewUserPage />
-          </PrivateRoute>
-        }
-      />
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/newuser"
+          element={
+            <PrivateRoute>
+              <NewUserPage />
+            </PrivateRoute>
+          }
+        />
 
-      <Route
-        path="/home"
-        element={
-          <PrivateRoute>
-            <HomePage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/notifications"
-        element={
-          <PrivateRoute>
-            <NotificationsPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <PrivateRoute>
-            <ProfilePage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/library"
-        element={
-          <PrivateRoute>
-            <GameLibrary />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/liked-games"
-        element={
-          <PrivateRoute>
-            <LikedGames />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/profile/:userId"
-        element={
-          <PrivateRoute>
-            <UserProfilePage />
-          </PrivateRoute>
-        }
-      />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/notifications"
+          element={
+            <PrivateRoute>
+              <NotificationsPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/library"
+          element={
+            <PrivateRoute>
+              <GameLibrary />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/liked-games"
+          element={
+            <PrivateRoute>
+              <LikedGames />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile/:userId"
+          element={
+            <PrivateRoute>
+              <UserProfilePage />
+            </PrivateRoute>
+          }
+        />
 
-      <Route
-        path="/profile/:userId/library"
-        element={
-          <PrivateRoute>
-            <UserGameLibrary />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/profile/:userId/library"
+          element={
+            <PrivateRoute>
+              <UserGameLibrary />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 export default App;
